fa59a76093623e6b0c57e1ee386df7f633aadfcb
Cody Schwarz <cody@simondata.com>
2019-02-28 12:30:30 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 from setuptools import setup
-
 setup(
     name="tap-greenhouse",
     version="0.1.0",
@@ -13,7 +12,10 @@ setup(
         "singer-python==5.2.0",
         'requests==2.18.4',
         "pendulum==1.2.0",
-        "tap-kit @ git+https://github.com/Radico/tap-kit.git@master"
+        "tap-kit==0.1.1"
+    ],
+    dependency_links=[
+        "https://github.com/Radico/tap-kit/tarball/master#egg=tap-kit-0.1.1",
     ],
     entry_points="""
     [console_scripts]
