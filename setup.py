d4ca9460255c1ba12f18d037b2877af995498e44
dmzobel <dmzobel@gmail.com>
2020-01-27 12:44:57 -0500

    update tap-kit install

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "singer-python==5.2.0",
         'requests==2.18.4',
         "pendulum==1.2.0",
-        "tap-kit==0.1.1"
+        "tap-kit @ git+https://github.com/Radico/tap-kit.git@master"
     ],
     dependency_links=[
         "https://github.com/Radico/tap-kit/tarball/master#egg=tap-kit-0.1.1",
